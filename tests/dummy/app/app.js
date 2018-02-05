e42d6afebc68d0e8c2e0e61e32d0e35decc6bd81
Bernardo Rittmeyer <bernardo@rittme.com>
2018-02-05 14:14:15 +0800

    Add test suite (#4)
    Add dummy app to test the addon

---
+++ tests/dummy/app/app.js
@@ -0,0 +1,14 @@
+import Ember from "ember";
+import loadInitializers from "ember-load-initializers";
+import Resolver from "./resolver";
+import config from "./config/environment";
+
+const App = Ember.Application.extend({
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
+  Resolver
+});
+
+loadInitializers(App, config.modulePrefix);
+
+export default App;
