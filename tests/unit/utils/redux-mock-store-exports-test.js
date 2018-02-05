41d0273468dccaf8af561d9a13ac326a66c06844
Bernardo Rittmeyer <bernardo@rittme.com>
2018-02-05 14:49:51 +0800

    Use beforeEach on tests (#5)

--- tests/unit/utils/redux-mock-store-exports-test.js
+++ tests/unit/utils/redux-mock-store-exports-test.js
@@ -2,32 +2,36 @@ import { test, module } from "qunit";
 
 import configureStore from "redux-mock-store";
 
-module("Unit | Util | Redux Mock Store Exports");
+module("Unit | Util | Redux Mock Store Exports", {
+  beforeEach() {
+    this.store = configureStore()();
+  }
+});
 
 test("exports configureStore", function(assert) {
   assert.equal(typeof configureStore, "function");
 });
 
 test("configureStore has dispatch", function(assert) {
-  assert.equal(typeof configureStore.dispatch, "function");
+  assert.equal(typeof this.store.dispatch, "function");
 });
 
 test("configureStore has getState", function(assert) {
-  assert.equal(typeof configureStore.getState, "function");
+  assert.equal(typeof this.store.getState, "function");
 });
 
 test("configureStore has getActions", function(assert) {
-  assert.equal(typeof configureStore.getActions, "function");
+  assert.equal(typeof this.store.getActions, "function");
 });
 
 test("configureStore has clearActions", function(assert) {
-  assert.equal(typeof configureStore.clearActions, "function");
+  assert.equal(typeof this.store.clearActions, "function");
 });
 
 test("configureStore has subscribe", function(assert) {
-  assert.equal(typeof configureStore.subscribe, "function");
+  assert.equal(typeof this.store.subscribe, "function");
 });
 
 test("configureStore has replaceReducer", function(assert) {
-  assert.equal(typeof configureStore.replaceReducer, "function");
+  assert.equal(typeof this.store.replaceReducer, "function");
 });
